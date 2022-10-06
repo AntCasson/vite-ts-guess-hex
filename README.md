0d0f3e006e8e5785c04fc02b40d51d9874803c97
AntCasson <59474291+AntCasson@users.noreply.github.com>
2022-10-06 22:37:03 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vite-ts-guess-hex
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-vz9hxh)
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="layout">
+      <div className="color-box"></div>
+      <button className="button">red</button>
+      <button className="button">blue</button>
+      <button className="button">green</button>
+    </div>
+  )
+}
+
+export default App
